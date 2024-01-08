864888dfae962953c0a254767e6175bb70d6abec
Hoàng Trung <trung@MacBook-Pros-Trung.local>
2024-01-08 15:37:12 +0700

    uodae

--- src/components/postInfo.tsx
+++ src/components/postInfo.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from 'react';
+interface ImageInfo {
+    image_path: string;
+}
+
 interface PostInfo {
     id: number;
     countLike: number;
     countReport: number;
     countComment: number;
     content: string;
+    images : ImageInfo[];
     isPublic: boolean;
     createdAt: string;
     updatedAt: string;
@@ -23,8 +29,17 @@ interface Props {
 }
 
 const PostInfoModal: React.FC<Props> = ({ post, show, handleClose }) => {
+    const [imagePaths, setImagePaths] = useState<string[]>([]);
+    useEffect(() => {
+        if (post && post.images && post.images.length > 0) {
+            const paths = post.images.map((image: ImageInfo) => image.image_path);
+            console.log("Post:", paths)
+            setImagePaths(paths);
+        }
+    }, [post]);
+
     if (!post) return null;
-    console.log("ok 1111:" , post);
+
     return (
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
@@ -34,7 +49,20 @@ const PostInfoModal: React.FC<Props> = ({ post, show, handleClose }) => {
                 <p>Nội dung: {post.content}</p>
                 <p>Số like: {post.countLike}</p>
                 <p>Số báo cáo: {post.countReport}</p>
-                <p>Ngày thay đổi thông tin gần nhất: {post.updatedAt}</p>
+                <p>Số bình luận: {post.countComment}</p>
+                <p>Ảnh:</p>
+                <div>
+                    {imagePaths.map((image_Path, index) => (
+                        <img
+                            key={index}
+                            src={image_Path}
+                            alt={`Image ${index}`}
+                            style={{maxWidth: '100%', maxHeight: '200px', marginBottom: '10px'}}
+                        />
+                    ))}
+                </div>
+
+                <p>Ngày thay đổi thông tin gần nhất: {post.updatedAt.substring(0,10)}</p>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
